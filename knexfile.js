f55108ce53d33a0addd6f6a25648dc4ef825354f
lesley <lesleybanadzem2017@gmail.com>
2020-01-28 15:16:38 -0800

    Refactored the production code .

--- knexfile.js
+++ knexfile.js
@@ -26,6 +26,11 @@ module.exports = {
   production: {
     client: 'pg',
     connection:  process.env.DATABASE_URL,
-    ...sqlite,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
   },
 }
